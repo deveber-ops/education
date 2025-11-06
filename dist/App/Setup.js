c1649c31dcef1c40f483378389885d781e7b1ebf
deveber-ops <deveber.dev@gmail.com>
2025-11-06 23:56:28 +0300

    Fix testing router on Setup.ts

--- dist/App/Setup.js
+++ dist/App/Setup.js
@@ -5,8 +5,6 @@ import { loadRoutes } from './Router.js';
 import { errorsMiddleware } from '../Core/Errors/errors.middleware.js';
 import database from '../Database/database.js';
 import * as dotenv from "dotenv";
-import { Blogs, Comments, Posts, Users } from '../Database/schema.js';
-import { HttpStatus } from '../Core/Types/httpStatuses.enum.js';
 dotenv.config();
 const setupApp = async (app) => {
   await database.connect();
@@ -16,18 +14,6 @@ const setupApp = async (app) => {
   app.use(detectClientTypeMiddleware);
   const { router } = await loadRoutes();
   app.use(router);
-  app.use("/api/testing/all-data", async (req, res, next) => {
-    const db = database.getDB();
-    try {
-      await db.delete(Comments).execute();
-      await db.delete(Posts).execute();
-      await db.delete(Users).execute();
-      await db.delete(Blogs).execute();
-      res.sendStatus(HttpStatus.NoContent);
-    } catch (error) {
-      next(error);
-    }
-  });
   app.use(errorsMiddleware);
   return app;
 };
---
+++ dist/Modules/Testing/config.js
@@ -0,0 +1,32 @@
+import database from '../../Database/database.js';
+import { Blogs, Comments, Posts, Users } from '../../Database/schema.js';
+import { HttpStatus } from '../../Core/Types/httpStatuses.enum.js';
+var config_default = {
+  name: "testing",
+  path: "/testing",
+  label: "\u0422\u0435\u0441\u0442\u0438\u0440\u043E\u0432\u0430\u043D\u0438\u0435",
+  actions: [
+    {
+      method: "DELETE",
+      name: "\u0423\u0434\u0430\u043B\u0435\u043D\u0438\u0435 \u0432\u0441\u0435\u0445 \u0434\u0430\u043D\u043D\u044B\u0445",
+      path: `/testing/all-data`,
+      middlewares: [],
+      handler: async (req, res, next) => {
+        const db = database.getDB();
+        try {
+          await db.delete(Comments).execute();
+          await db.delete(Posts).execute();
+          await db.delete(Blogs).execute();
+          await db.delete(Users).execute();
+          res.sendStatus(HttpStatus.NoContent);
+        } catch (error) {
+          next(error);
+        }
+      },
+      authorization: false
+    }
+  ]
+};
+export {
+  config_default as default
+};
