98e0c3df4897bee00bacd375d89bb83c55a313fe
deveber-ops <deveber.dev@gmail.com>
2025-11-07 00:58:16 +0300

    Fix authMiddleware. Added Basic authorization.

--- dist/Modules/Auth/Middlewares/auth.middleware.js
+++ dist/Modules/Auth/Middlewares/auth.middleware.js
@@ -3,28 +3,46 @@ import { authError } from '../../../Core/Errors/auth.errors.js';
 import * as dotenv from "dotenv";
 dotenv.config();
 const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET;
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 const extractToken = (req) => {
+  const auth = req.headers.authorization;
   let accessToken;
-  const authHeader = req.headers.authorization;
-  if (authHeader && authHeader.startsWith("Bearer ")) {
-    accessToken = authHeader.substring(7);
+  let basicAuth = false;
+  const [authType, authToken] = auth.split(" ");
+  if (authType === "Bearer") {
+    accessToken = authToken;
+  }
+  if (authType === "Basic") {
+    accessToken = authToken;
+    basicAuth = true;
   }
   if (!accessToken && req.cookies?.accessToken) {
     accessToken = req.cookies.accessToken;
   }
-  return { accessToken };
+  return { accessToken, basicAuth };
 };
 const authMiddleware = async (req, res, next) => {
-  const { accessToken } = extractToken(req);
+  const { accessToken, basicAuth } = extractToken(req);
   if (!accessToken) return next(new authError("\u0422\u043E\u043A\u0435\u043D \u0430\u0432\u0442\u043E\u0440\u0438\u0437\u0430\u0446\u0438\u0438 \u043D\u0435 \u043F\u0435\u0440\u0435\u0434\u0430\u043D.", "token"));
-  try {
-    const payload = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
-    req.userId = Number(payload.sub);
-    return next();
-  } catch (error) {
-    return next(new authError("\u0422\u043E\u043A\u0435\u043D \u0430\u0432\u0442\u043E\u0440\u0438\u0437\u0430\u0446\u0438\u0438 \u043D\u0435\u0434\u0435\u0439\u0441\u0442\u0432\u0438\u0442\u0435\u043B\u0435\u043D.", "token"));
+  if (basicAuth) {
+    const credentials = Buffer.from(accessToken, "base64").toString("utf-8");
+    const [username, password] = credentials.split(":");
+    if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
+      next(new authError("\u041D\u0435 \u0430\u0432\u0442\u043E\u0440\u0438\u0437\u043E\u0432\u0430\u043D.", "token"));
+    }
+  } else {
+    try {
+      const payload = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
+      req.userId = Number(payload.sub);
+      return next();
+    } catch (error) {
+      return next(new authError("\u0422\u043E\u043A\u0435\u043D \u0430\u0432\u0442\u043E\u0440\u0438\u0437\u0430\u0446\u0438\u0438 \u043D\u0435\u0434\u0435\u0439\u0441\u0442\u0432\u0438\u0442\u0435\u043B\u0435\u043D.", "token"));
+    }
   }
 };
 export {
+  ADMIN_PASSWORD,
+  ADMIN_USERNAME,
   authMiddleware
 };
--- src/Modules/Auth/Middlewares/auth.middleware.ts
+++ src/Modules/Auth/Middlewares/auth.middleware.ts
@@ -5,24 +5,34 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET!;
+export const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 export interface AuthenticatedRequest extends Request {
     userId?: number;
 }
 
-const extractToken = (req: Request): { accessToken?: string } => {
-    let accessToken: string | undefined;
+const extractToken = (req: Request) => {
+    const auth = req.headers.authorization as string
 
-    const authHeader = req.headers.authorization;
-    if (authHeader && authHeader.startsWith('Bearer ')) {
-        accessToken = authHeader.substring(7);
+    let accessToken: string | undefined;
+    let basicAuth: boolean = false;
+
+    const [authType, authToken] = auth.split(' ')
+
+    if (authType === 'Bearer') {
+        accessToken = authToken
+    }
+    if (authType === 'Basic') {
+        accessToken = authToken
+        basicAuth = true
     }
 
     if (!accessToken && req.cookies?.accessToken) {
         accessToken = req.cookies.accessToken;
     }
 
-    return { accessToken };
+    return { accessToken, basicAuth };
 };
 
 export const authMiddleware = async (
@@ -30,14 +40,23 @@ export const authMiddleware = async (
     res: Response,
     next: NextFunction
 ): Promise<Response | void> => {
-    const { accessToken } = extractToken(req);
+    const { accessToken, basicAuth } = extractToken(req);
     if (!accessToken) return next(new authError('Токен авторизации не передан.', 'token'))
 
-    try {
-        const payload = jwt.verify(accessToken, ACCESS_TOKEN_SECRET) as any;
-        (req as any).userId = Number(payload.sub);
-        return next();
-    } catch (error) {
-        return next(new authError('Токен авторизации недействителен.', 'token'))
+    if (basicAuth) {
+        const credentials = Buffer.from(accessToken, 'base64').toString('utf-8');
+        const [username, password] = credentials.split(':')
+
+        if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
+            next(new authError('Не авторизован.', 'token'))
+        }
+    } else {
+        try {
+            const payload = jwt.verify(accessToken, ACCESS_TOKEN_SECRET) as any;
+            (req as any).userId = Number(payload.sub);
+            return next();
+        } catch (error) {
+            return next(new authError('Токен авторизации недействителен.', 'token'))
+        }
     }
 };
